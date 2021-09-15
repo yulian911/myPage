860dabe418e787aa412d591e4ddd93328f21848d
yulian911 <74702716+yulian911@users.noreply.github.com>
2021-09-15 22:54:48 +0200

    new site

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Container } from "./components/module/Containers";
+import Navigation from "./components/Header/Navigation";
+import { Route, Switch } from "react-router";
+import Home from "./components/section/Home";
+import About from "./components/section/About";
+import Skills from "./components/section/Skills";
+import Gallery from "./components/section/Gallery";
+import Contact from "./components/section/Contact";
+import ScrollToTop from "./components/scrollToTop";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <ScrollToTop /> */}
+      <Navigation />
+
+      {/* <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/skills" component={Skills} />
+      <Route path="/gallery" component={Gallery} />
+      <Route path="/contact" component={Contact} /> */}
+
+      <Home id="/home" />
+      <About id="/about" />
+    </>
   );
 }
 
---
+++ src/components/Header/MobileStyle.tsx
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+
+export interface IStyle {
+  isScroll: boolean | null;
+  click: boolean | null;
+  isResize: boolean | null;
+}
+
+export const NavMobile = styled.ul`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  /* align-items: center; */
+  top: 0;
+  left: ${(props: IStyle) => (props.click ? "0" : "-100%")};
+  background-color: black;
+  width: 100%;
+  height: 100vh;
+  transition: all 0.3s ease;
+`;
+export const NavMobileList = styled.li`
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  padding: 10px 0px;
+  /* background-color: yellow; */
+`;
+
+export const HeaderMobile = styled.div`
+  position: fixed;
+  z-index: 1000;
+  width: 100%;
+  height: 60px;
+  display: flex;
+  /* justify-content: flex-end; */
+  transition: all 0.3s ease;
+`;
+export const NavMobileContainer = styled.div`
+  /* background-color: red; */
+  width: 100%;
+  display: flex;
+  z-index: 999;
+  justify-content: flex-end;
+  align-items: center;
+  margin-right: 25px;
+  transition: all 0.3s ease;
+`;
+
+export const LogoMobile = styled.div`
+  a {
+    text-decoration: none;
+    color: white;
+    font-size: 35px;
+    font-weight: 600;
+    transition: all 0.3s ease;
+    span {
+      color: crimson;
+      transition: all 0.3s ease;
+    }
+  }
+`;
